3eeb6145e046254f33345f2f6fe228011c75127e
Pnski <s1l3nce@hotmail.de>
2025-01-11 15:51:32 +0100

    _u

--- scripts/skillsfjson.py
+++ scripts/skillsfjson.py
@@ -1,34 +1,34 @@
 import os
 import json
 
-def compare_filenames(directory, json_string):
-    # Parse the JSON string into a Python list
-    json_filenames = json.loads(json_string)
-    
-    # Get all filenames in the specified directory
-    directory_filenames = os.listdir(directory)
-    
-    # Compare the filenames from the directory to the JSON list
+directory = 'E:\\Github\\TLLib\\docs\\Images\\Active'
+jsonFile = 'D:\\TLSKill.json'
+
+def process_filenames():
+    # Read filenames and process them
+    filenames = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    processed_filenames = [f[8:].rsplit('.', 1)[0] for f in filenames]
+
+    # Load JSON data
+    with open(jsonFile, 'r') as file:
+        json_data = json.load(file)
+
+    if isinstance(json_data, list) and 'Rows' in json_data[0]:
+        json_data = json_data[0]['Rows']
+
     matches = []
-    differences = []
+    for filename in filenames:
+        for key, value in json_data.items():
+            if key.startswith('WP') and filename[8:].rsplit('.', 1)[0] in key:
+                matches.append(f"| <img src=\"../images/Active/{filename}\"> | {key} | {value.get('damage_type', 'N/A')} |")
+    #<img src="./Item_128/Usable/abyss_point_charge_001_1_A.png">
 
-    for filename in json_filenames:
-        if filename in directory_filenames:
-            matches.append(filename)
-        else:
-            differences.append(filename)
+
+    output_file='dmgType.md'
     
-    for filename in directory_filenames:
-        if filename not in json_filenames:
-            differences.append(filename)
-    
-    return matches, differences
+    with open(output_file, 'w') as file:
+        file.write("| Ico | Name | DamageType |\n")
+        file.write("| --- | --- | --- |\n")
+        file.write('\n'.join(matches) + '\n')
 
-# Example usage:
-directory = 'D:\FModel\4.4.4\Output\Exports\TL\Content\Image\Skill'  # Replace with your directory path
-json_string = '["file1.txt", "file2.jpg", "file3.pdf"]'  # Replace with your JSON string
-
-matches, differences = compare_filenames(directory, json_string)
-
-print(f"Matching filenames: {matches}")
-print(f"Different filenames: {differences}")
+process_filenames()
