b75a15e36dfa718b2785af64933fe911ea7d03c5
pnski <s1l3nce@hotmail.de>
2025-04-22 18:03:30 +0200

    _u

--- scripts/amitoi.py
+++ scripts/amitoi.py
@@ -10,13 +10,19 @@ def loadFile(filepath):
 regionPath = "sources/TLMagicDollExpeditionRegion.json"
 groupPath = "sources/TLItemLotteryPrivateGroup.json"
 unitPath = "sources/TLItemLotteryUnit.json"
+imgPath = "sources/TLItemLooks.json"
 
 regionData = loadFile(regionPath)
 groupData = loadFile(groupPath)
 unitData = loadFile(unitPath)
+imgData = loadFile(imgPath)
 
 def avgQuantity(entries):
     return sum(entry['quantity'] * (entry['prob'] / 10000) for entry in entries) / sum(entry['prob'] / 10000 for entry in entries)
+
+def getImg(item):
+    rPath = imgData[item]["IconPath"]["AssetPathName"].replace("/Game", "").replace(".I_", "/") + ".png"
+    return f"<img='.{rPath}'>"
 
 def writeMarkdown(output):
     with open(output, "w", encoding="utf-8") as md:
@@ -32,7 +38,7 @@ def writeMarkdown(output):
                     if gEntry["id"] in unitData:
                         ItemLotteryUnitEntry = unitData[gEntry["id"]]["ItemLotteryUnitEntry"]
                         regionRewards.append({
-                            'item': ItemLotteryUnitEntry[0]["item"],
+                            'item': getImg(ItemLotteryUnitEntry[0]["item"]),
                             'avg': avgQuantity(ItemLotteryUnitEntry)
                         })
                         #print("✅", gEntry["id"], unitData[gEntry["id"]])
@@ -44,31 +50,6 @@ def writeMarkdown(output):
 
         md.write("All Propabilitys are averages\n\n")
         md.write("sources = "+", ".join([regionPath,groupPath,unitPath]))
-
-    replacements = {
-    "dungeon_point_stone_05_A": "<img src='./Image/Icon/Item_128/Usable/abyss_point_charge_001_1_A.png'>",
-    "material_mana_fabric_02": "<img src='./Image/Icon/Item_128/Misc/I_ManaFabric_002.png'>",
-    "material_mana_wood_02": "<img src='./Image/Icon/Item_128/Misc/I_ManaWood_002.png'>",
-    "Material_Food_Sub_Rotein_002": "<img src='./Image/Icon/Item_128/Usable/I_food_sub_045.png'>",
-    "material_mana_leather_02":"<img src='./Image/Icon/Item_128/Misc/I_ManaLeather_002.png'>",
-    "material_mana_steel_02": "<img src='./Image/Icon/Item_128/Misc/I_ManaSteel_002.png'>",
-    "Material_Food_Sub_Solute_002":"<img src='./Image/Icon/Item_128/Usable/I_food_sub_031.png'>",
-    "material_gold_02":"<img src='./Image/Icon/Item_128/Misc/I_Gold_002.png'>",
-    "Material_Food_Sub_Fruice_003":"<img src='./Image/Icon/Item_128/Usable/I_food_sub_018.png'>",
-    "material_food_main_032":"<img src='./Image/Icon/Item_128/Usable/I_material_food_main_032.png'>",
-    "Material_Food_Sub_Herba_003":"<img src='./Image/Icon/Item_128/Usable/I_food_sub_025.png'>",
-    "material_food_main_033":"<img src='./Image/Icon/Item_128/Usable/I_material_food_main_033.png'>",
-    "material_food_main_012":"<img src='./Image/Icon/Item_128/Usable/I_material_food_main_012.png'>",
-    }
-
-    with open(output, "r", encoding="utf-8") as f:
-        content = f.read()
-
-    for old, new in replacements.items():
-        content = content.replace(old, new)
-
-    with open(output, "w", encoding="utf-8") as f:
-        f.write(content)
 
 writeMarkdown(
     output="docs/doll/expeditionRewards.md"
