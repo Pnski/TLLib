cbbebdb1717c1f93cbf3c42ea0d5eb5d301c66e3
pnski <s1l3nce@hotmail.de>
2025-06-30 12:57:01 +0200

    _u skills

--- scripts/skills.py
+++ scripts/skills.py
@@ -18,23 +18,13 @@ sType = loadFile('sources/TLSkill.json')
 
 for fName in os.listdir(skillsFolder):
     ffName = os.path.join(skillsFolder, fName)
-    _json = loadFile(ffName)
-    for row_name, name in _json.items():
-        if row_name in sType:
-            print("rowname FOUND",row_name)
-        else:
-            print("NOTFOUND",row_name)
-
-
-""" for fName in os.listdir(skillsFolder):
-    ffName = os.path.join(skillsFolder, fName)
     oName = os.path.join(oFolder, ffName.split("_")[-1].split(".")[0]) + ".md"
     _json = loadFile(ffName)
     
     os.makedirs(os.path.dirname(oName), exist_ok=True)
     with open(oName, "w", encoding="utf-8") as md:
         md.write(f"# {ffName.split('_')[-1].split('.')[0]} Skill Types\n\n")
-        head = ['SkillName','SkillInternal','SkillPicture','SkillType']
+        head = ['SkillName','SkillInternal','SkillPicture','SkillType', 'SkillDelay','HitDelay','MaxChargeDelay']
         md.write("| " + " | ".join(head) + " |\n")
         md.write("| " + " | ".join(['-' * 3 for h in head]) + " |\n")
 
@@ -43,7 +33,6 @@ for fName in os.listdir(skillsFolder):
             if path.startswith("/Game/Image/Skill/Active/") and "WeaponSpec" not in path:
                 if row_name in sType:
                     if row.get('UIName').get('LocalizedString'):
-                        md.write(f"| {row['UIName']['LocalizedString']} | {row_name} | {getImg(row)} | {sType[row_name]['damage_type'].split('::k')[-1]} |\n")
+                        md.write(f"| {row['UIName']['LocalizedString']} | {row_name} | {getImg(row)} | {sType[row_name]['damage_type'].split('::k')[-1]} | {row[row_name]['skill_delay']} | {row[row_name]['hit_delay']} | {row[row_name]['max_charge_delay']} |\n")
         md.write("\n\n")
         md.write("sources = "+fName)
-         """
