bdcf635a927b6fe06201ea6a7041dcd2dbd94bf0
pnski <s1l3nce@hotmail.de>
2025-09-10 23:44:15 +0200

    _u improving readability of buttons

--- docs/calculator/scripts/groupbuffs.js
+++ docs/calculator/scripts/groupbuffs.js
@@ -487,7 +487,7 @@ function injectStyles() {
     style.textContent = `
         #buttons {
             display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
             gap: 6px;
             margin-top: 10px;
         }
